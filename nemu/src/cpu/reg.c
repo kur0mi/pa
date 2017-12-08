4ad2fa86db55272ea35879a8f015895feebaa4e5
tracer-ics2017 <tracer@njuics.org>
2017-12-08 11:06:23 +0000

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  11:06:23 up 6 days,  7:58,  4 users,  load average: 0.00, 0.00, 0.00 d25b489b41250101c22725054ca29e360e9ed57

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -18,6 +18,7 @@ void reg_test() {
   for (i = R_EAX; i <= R_EDI; i ++) {
     sample[i] = rand();
     reg_l(i) = sample[i];
+	printf("%d", sample[i]);
     assert(reg_w(i) == (sample[i] & 0xffff));
   }
 
