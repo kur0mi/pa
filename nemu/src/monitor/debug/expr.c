6e4332d216ecea324dc916957d540e6e1366eab2
tracer-ics2017 <tracer@njuics.org>
2018-01-21 22:52:59 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  22:52:59 up 50 days, 11:45,  3 users,  load average: 0.00, 0.00, 0.00 6222c522639e5dfcd9b634b133a4b9c6dc0a6fde

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -176,6 +176,9 @@ uint32_t eval(int p, int q){
 	}
 	else{
 		int domi = get_dominant(p, q);
+		if (tokens[domi].type == TK_NEGTIVE)
+			return -eval(domi+1, q);
+
 		int val1 = eval(p, domi-1);
 		int val2 = eval(domi+1, q);
 		switch (tokens[domi].type){
