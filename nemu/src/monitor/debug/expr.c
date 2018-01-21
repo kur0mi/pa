2526ec781f8d54318b7431a15f0df46b0c610c71
tracer-ics2017 <tracer@njuics.org>
2018-01-21 23:54:17 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  23:54:17 up 50 days, 12:46,  2 users,  load average: 0.00, 0.00, 0.00 1d0cea03f0dbc2c1ca467e2c3cb73c0c8dc3096

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -179,7 +179,7 @@ int get_dominant(int p, int q){
 		}
 	}
 
-	//Log("hit level %d, %d - %d", min_level, p, q);
+	Log("hit level %d, %d - %d", min_level, p, q);
 	Assert(domi != -1, "cannot hit dominant");
 	return domi;
 }
