b2aca0ae936eb0d965ca0daf07b3707c0eedc2f7
tracer-ics2017 <tracer@njuics.org>
2018-01-22 00:16:14 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  00:16:14 up 50 days, 13:08,  1 user,  load average: 0.00, 0.00, 0.00 7cf254a7b3fb986b3c481279297eb1a16d2d1fe1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -98,7 +98,7 @@ static bool make_token(char *e) {
 				tokens[nr_token].type = rules[i].token_type;
 				break;
 		  	case '-':
-				if (position == substr_len || check_negtive_prefixx(i-1)){
+				if (nr_token == 0 || check_negtive_prefixx(i-1)){
 					tokens[nr_token].type = TK_NEGTIVE;
 					Log("negtive");
 				}
