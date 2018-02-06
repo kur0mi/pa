f0230333379e92e65271f04bc57840e3f4f84c55
tracer-ics2017 <tracer@njuics.org>
2018-02-06 18:29:22 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  18:29:22 up 66 days,  7:21,  1 user,  load average: 0.00, 0.00, 0.00 65411253f18f8c3e662b43ceb708b109ec7b82fb

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,5 +1,4 @@
 #include "nemu.h"
-#include "monitor/expr.h"
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
