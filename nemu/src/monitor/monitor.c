60fe810317daab143cb51c7c865f4a8e2ade25f7
tracer-ics2017 <tracer@njuics.org>
2018-02-11 16:50:26 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  16:50:26 up 71 days,  5:42,  1 user,  load average: 0.00, 0.00, 0.00 2acc364e59867e40ffbfc509b974a93ba7e7c6d5

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -41,6 +41,7 @@ static inline int load_default_img() {
     0xb9, 0xff, 0x00, 0x00, 0x00,        	// movl  $0xff, %ecx
     0x89, 0b00000001,                       // movl  %eax, (%ecx)
 	0x89, 0b00000100, 0b10001001, 			// movl  %eax, (%ecx, %ecx, 4)
+	0x89, 0b00000101, 0x01, 0x01, 0x01, 0x01, 	// movl  %eax, 0x01010101() 
 	0x89, 0b01000001, 0x04, 				// movl  %eax, 0x4(%ecx)
 	0x89, 0b01000100, 0b10001001, 0x04, 	// movl  %eax, 0x4(%ecx, %ecx, 4)
     
