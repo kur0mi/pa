1768a42b99a485adb70a6f751ffe09cfb6a5a9c0
tracer-ics2017 <tracer@njuics.org>
2018-02-18 20:02:19 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  20:02:19 up 78 days,  8:54,  1 user,  load average: 0.01, 0.01, 0.00 868c010193d83ff87d0d9501fbed2b0bab32b0ab

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -183,8 +183,11 @@ static inline void rtl_sext(rtlreg_t * dest, const rtlreg_t * src1, int width)
 
 static inline void rtl_push(const rtlreg_t * src1, int width)
 {
-    printf("[src1]: 0x%08x\n", src1);
-    printf("[src1]: 0x%08x\n", *src1);
+    printf("[*src1]: 0x%08x\n", *src1);
+    printf("[width]: %d\n", width);
+    printf("[esp]: 0x%08x\n", cpu.esp);
+
+
 
 	// esp <- esp - 4
 	// M[esp] <- src1
