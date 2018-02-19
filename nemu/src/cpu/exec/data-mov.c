e66b697c2e81418dccba34a7395eb3f8f57f5b75
tracer-ics2017 <tracer@njuics.org>
2018-02-19 16:05:57 +0800

    >  clean 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  16:05:57 up 79 days,  4:58,  5 users,  load average: 0.00, 0.00, 0.00 1249ae86a48c237a9b239634754476317cb1289d

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -1,4 +1,5 @@
 #include "cpu/exec.h"
+	printf("[addr]: 0x%08x\n", addr);
 
 make_EHelper(mov)
 {
@@ -37,7 +38,6 @@ make_EHelper(push)
 
 make_EHelper(pop)
 {
-	printf("[id_dest->addr]: 0x%08x\n", id_dest->addr);
     uint32_t * addr;
 	int width = id_dest->width;
 	if (id_dest->type == OP_TYPE_MEM)// || OP_TYPE_REG || OP_TYPE_IMM)
@@ -45,8 +45,7 @@ make_EHelper(pop)
 	else
 		panic("unknown type");
 #ifdef EXT_DEBUG
-	printf("[addr]: 0x%08x\n", addr);
-	printf("[*addr]: 0x%08x\n", *addr);
+	//printf("[addr]: 0x%08x\n", addr);
 	printf("[width]: %d\n", width);
 	printf("[esp]: 0x%08x\n", cpu.esp);
 #endif
