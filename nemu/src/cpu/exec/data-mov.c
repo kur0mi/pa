8757db3c7f4ebb16feb264914822272d75b34556
tracer-ics2017 <tracer@njuics.org>
2018-02-19 15:52:46 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  15:52:46 up 79 days,  4:45,  5 users,  load average: 0.00, 0.00, 0.00 719ad144e618e2e9e3bca4389eb22ee027b8b3ac

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -45,6 +45,7 @@ make_EHelper(pop)
 		panic("unknown type");
 #ifdef EXT_DEBUG
 	printf("[addr]: 0x%08x\n", addr);
+	printf("[*addr]: 0x%08x\n", *addr);
 	printf("[width]: %d\n", width);
 	printf("[esp]: 0x%08x\n", cpu.esp);
 #endif
