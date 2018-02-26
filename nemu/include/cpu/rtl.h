a679032a6cbf15888ce39fbc2ee2d4124c380d3d
tracer-ics2017 <tracer@njuics.org>
2018-02-26 20:28:13 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  20:28:13 up 86 days,  9:20, 25 users,  load average: 0.00, 0.00, 0.00 61997c0857b18b394c552a5c584e01acb06fb8d7

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -224,7 +224,7 @@ static inline void rtl_not(rtlreg_t * dest)
 	*dest = ~(*dest); 
 }
 
-// 符号拓展
+// 符号位扩展
 static inline void rtl_sext(rtlreg_t * dest, const rtlreg_t * src1, int width)
 {
 	// dest <- signext(src1[(width * 8 - 1) .. 0])
