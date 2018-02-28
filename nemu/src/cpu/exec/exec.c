066d749e7804184693ab033c4c0433143c35da65
tracer-ics2017 <tracer@njuics.org>
2018-02-28 21:37:29 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  21:37:29 up 88 days, 10:29, 28 users,  load average: 0.00, 0.00, 0.00 cc750fd07b5e31b1f455168d86efd070f6bb794

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -48,7 +48,7 @@ static make_EHelper(name) { \
     }
 */
 /* 0x80, 0x81, 0x83 */
-make_group(gp1, EMPTY, EMPTY, EMPTY, EMPTY, EX(and), EX(sub), EMPTY, EMPTY)
+make_group(gp1, EX(add), EMPTY, EMPTY, EMPTY, EX(and), EX(sub), EMPTY, EMPTY)
 
     /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
     make_group(gp2, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY)
@@ -67,8 +67,8 @@ make_group(gp1, EMPTY, EMPTY, EMPTY, EMPTY, EX(and), EX(sub), EMPTY, EMPTY)
 
 /* TODO: Add more instructions!!! */
 opcode_entry opcode_table[512] = {
-	/* 0x00 */ EMPTY, EMPTY, EMPTY, IDEX(E2G, add),
-	/* 0x04 */ EMPTY, EMPTY, EMPTY, EMPTY,
+	/* 0x00 */ IDEXW(G2E, add, 1), IDEX(G2E, add), IDEXW(E2G, add, 1), IDEX(E2G, add),
+	/* 0x04 */ IDEXW(I2a, add, 1), IDEX(I2a, add), EMPTY, EMPTY,
 	/* 0x08 */ EMPTY, EMPTY, EMPTY, EMPTY,
 	/* 0x0c */ EMPTY, EMPTY, EMPTY, EX(2byte_esc),
 	/* 0x10 */ EMPTY, EMPTY, EMPTY, EMPTY,
@@ -77,8 +77,8 @@ opcode_entry opcode_table[512] = {
 	/* 0x1c */ EMPTY, EMPTY, EMPTY, EMPTY,
 	/* 0x20 */ EMPTY, EMPTY, EMPTY, EMPTY,
 	/* 0x24 */ EMPTY, EMPTY, EMPTY, EMPTY,
-	/* 0x28 */ EMPTY, EMPTY, EMPTY, EMPTY,
-	/* 0x2c */ EMPTY, EMPTY, EMPTY, EMPTY,
+	/* 0x28 */ IDEXW(G2E, sub, 1), IDEX(G2E, sub), IDEXW(E2G, sub, 1), IDEX(E2G, sub),
+	/* 0x2c */ IDEXW(I2a, sub, 1), IDEX(I2a, sub), EMPTY, EMPTY,
 	/* 0x30 */ IDEXW(G2E, xor, 1), IDEX(G2E, xor), EMPTY, EMPTY,
 	/* 0x34 */ EMPTY, EMPTY, EMPTY, EMPTY,
 	/* 0x38 */ EMPTY, EMPTY, EMPTY, EMPTY,
