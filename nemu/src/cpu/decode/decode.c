ae727273b31663b10141fc35457533da710cbec7
tracer-ics2017 <tracer@njuics.org>
2018-03-01 13:36:51 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  13:36:51 up 89 days,  2:29, 27 users,  load average: 0.00, 0.00, 0.00 19d34a95c497032cd1de82146d2403a6f2f12578

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -174,6 +174,15 @@ make_DHelper(lea_M2G)
 	decode_op_rm(eip, id_src, false, id_dest, false);
 }
 
+make_DHelper(movzx_E2G)
+{
+	if (decoding.opcode & 0x1)
+		id_src->width = 2;
+	else
+		id_src->width = 1;
+	decode_op_rm(eip, id_src, true, id_dest, false);
+}
+
 /* AL <- Ib
  * eAX <- Iv
  */
