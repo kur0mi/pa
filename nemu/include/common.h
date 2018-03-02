6e0b8aebc2be968a8f37600c13f301419a3bd378
tracer-ics2017 <tracer@njuics.org>
2018-03-02 23:19:14 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  23:19:14 up 90 days, 12:11, 27 users,  load average: 0.11, 0.03, 0.01 1685814588bf6f927d4191242386422d26a0b021

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -26,13 +26,11 @@ typedef uint16_t ioaddr_t;
 #define false 0
 #define true 1
 
-/*
 char *strlwr(char *str){
 	char *orign=str;
 	for (; *str!='\0'; str++)
 	*str = (char)((int)(*str) + 32);
 	return orign;
 }
-*/
 
 #endif
