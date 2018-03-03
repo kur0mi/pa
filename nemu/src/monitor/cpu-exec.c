50a832709fcd80c5fc02a571eb959bb0bf86d5e5
tracer-ics2017 <tracer@njuics.org>
2018-03-03 13:07:41 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  13:07:41 up 91 days,  2:00, 27 users,  load average: 0.00, 0.02, 0.01 51f9cd2ae1b0b632b8730eef3ce01530685e9e35

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -7,7 +7,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INSTR_TO_PRINT 100
+#define MAX_INSTR_TO_PRINT 200
 
 int nemu_state = NEMU_STOP;
 
@@ -22,7 +22,7 @@ void cpu_exec(uint64_t n)
 	}
 	nemu_state = NEMU_RUNNING;
 
-	bool print_flag = (int64_t) n < MAX_INSTR_TO_PRINT;
+	bool print_flag = n < MAX_INSTR_TO_PRINT;
 
 	for (; n > 0; n--) {
 		/* Execute one instruction, including instruction fetch,
