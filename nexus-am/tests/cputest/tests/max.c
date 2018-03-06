2e5d6c9352974253277b3b45516f7b819e52bce4
tracer-ics2017 <tracer@njuics.org>
2018-03-06 23:06:44 +0800

    >  run 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  23:06:44 up 94 days, 11:59, 28 users,  load average: 0.07, 0.03, 0.01 2938b9fc48745f161c8a3cd548a341d0d0c2fc8e

--- nexus-am/tests/cputest/tests/max.c
+++ nexus-am/tests/cputest/tests/max.c
@@ -13,6 +13,8 @@ int ans[] = {0, 0x1, 0x2, 0x7fffffff, 0, 0, 0, 0, 0x1, 0x1, 0x2, 0x7fffffff, 0x1
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
+	nemu_assert(max(0, 0x80000000) == 0);
+/*
 	int i, j, ans_idx = 0;
 	for(i = 0; i < NR_DATA; i ++) {
 		for(j = 0; j < NR_DATA; j ++) {
@@ -22,6 +24,6 @@ int main() {
 	}
 
 	nemu_assert(i == NR_DATA);
-
+*/
 	return 0;
 }
