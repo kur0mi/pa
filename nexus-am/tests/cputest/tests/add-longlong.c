3a92395267d25dabf8b5986fafce6e539b7d4b6a
tracer-ics2017 <tracer@njuics.org>
2018-03-08 15:08:30 +0800

    >  run 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  15:08:30 up 96 days,  4:00, 28 users,  load average: 0.01, 0.00, 0.00 3d5ffe504905810296ba09c81ca550d422b6ecb

--- nexus-am/tests/cputest/tests/add-longlong.c
+++ nexus-am/tests/cputest/tests/add-longlong.c
@@ -11,14 +11,14 @@ long long ans[] = {0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-/*
+
 	int i, j, ans_idx = 0;
 	for(i = 0; i < NR_DATA; i ++) {
 		for(j = 0; j < NR_DATA; j ++) {
 			nemu_assert(add(test_data[i], test_data[j]) == ans[ans_idx ++]);
 		}
 	}
-*/
+
 /*
 	int i, j;
 	for(i = 0; i < 3; i ++) {
@@ -27,7 +27,7 @@ int main() {
 		}
 	}
 */
-	nemu_assert(add(test_data[1], test_data[3]) == ans[4]);
+	//nemu_assert(add(test_data[1], test_data[3]) == ans[4]);
 
 	return 0;
 }
