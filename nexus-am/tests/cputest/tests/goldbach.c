dc986db5dc97556a4c9ace0876c785b9b052f193
tracer-ics2017 <tracer@njuics.org>
2018-03-08 21:17:50 +0800

    >  run 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  21:17:50 up 96 days, 10:10, 28 users,  load average: 0.10, 0.03, 0.01 85d1e60915d1fd6d42d2bc2ae36fd438db965c5c

--- nexus-am/tests/cputest/tests/goldbach.c
+++ nexus-am/tests/cputest/tests/goldbach.c
@@ -1,6 +1,6 @@
 #include "trap.h"
 
-// 2, 3, 5, 7			return 1
+// 2, 3, 5, 7
 int is_prime(int n) {
 	if(n < 2) return 0;
 
@@ -26,18 +26,6 @@ int goldbach(int n) {
 }
 
 int main() {
-	nemu_assert(goldbach(6) == 1);
-	//nemu_assert(is_prime());
-	//nemu_assert(1 && 1 == 1);
-	//nemu_assert(is_prime(2) == 1);
-	//nemu_assert(is_prime(3) == 1);
-	//nemu_assert(is_prime(5) == 1);
-	//nemu_assert(is_prime(7) == 1);
-
-	return 0;
-}
-/*
-int main() {
 	int n;
 	for(n = 4; n <= 30; n += 2) {
 		nemu_assert(goldbach(n) == 1);
@@ -45,5 +33,4 @@ int main() {
 
 	return 0;
 }
-*/
 
