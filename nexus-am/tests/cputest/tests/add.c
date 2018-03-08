d90aa432dcb8bae010cf406fefb560cd615c36d0
tracer-ics2017 <tracer@njuics.org>
2018-03-08 13:40:31 +0800

    >  run 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  13:40:31 up 96 days,  2:32, 28 users,  load average: 0.08, 0.02, 0.01 efaf4cbca6a5d1c49ed8b1b394bc6211be74898

--- nexus-am/tests/cputest/tests/add.c
+++ nexus-am/tests/cputest/tests/add.c
@@ -11,29 +11,15 @@ int ans[] = {0, 0x1, 0x2, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffff
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-/*
-	int i, j;
-	for (i = 0; i < NR_DATA; i++) {
-		for (j = 0; j < NR_DATA; j++) {
-			nemu_assert(add(test_data[i], test_data[j]) == ans[i * NR_DATA + j]);
+	int i, j, ans_idx = 0;
+	for(i = 0; i < NR_DATA; i ++) {
+		for(j = 0; j < NR_DATA; j ++) {
+			nemu_assert(add(test_data[i], test_data[j]) == ans[ans_idx ++]);
 		}
-	}
-*/
-
-	int i, j;
-	for(i = 0; i < 6; i ++) {
-		for(j = 0; j < 6; j ++) {
-			//ans_idx ++;
-			//nemu_assert(add(test_data[i], test_data[j]) == ans[ans_idx ++]);
-		}
-		nemu_assert(j == 6);
+		nemu_assert(j == NR_DATA);
 	}
 
-	nemu_assert(i == 6);
+	nemu_assert(i == NR_DATA);
 
-/*	int j;
-	for(j = 0; j < NR_DATA; j ++) {}
-	nemu_assert(j == NR_DATA);
-*/
 	return 0;
 }
