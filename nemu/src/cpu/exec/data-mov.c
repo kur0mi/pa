4d8263f2614bb56d7ddaa984bdaaa43ccba2c5c1
tracer-ics2017 <tracer@njuics.org>
2018-05-02 11:14:22 +0800

    >  compile 161620218 kur0mi Linux debian 4.4.0-102-generic #125-Ubuntu SMP Tue Nov 21 15:15:11 UTC 2017 x86_64 GNU/Linux  11:14:22 up 4 days, 20:10, 35 users,  load average: 0.00, 0.00, 0.00 872e9014e415d93c8039513097af97d7a94d5692

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -86,14 +86,14 @@ make_EHelper(pusha)
 
 	print_asm("pusha");
 #ifdef DEBUG
-	assert(cpu.eax == vaddr_read(cpu.esp - 4, 4));
-	assert(cpu.ecx == vaddr_read(cpu.esp - 8, 4));
-	assert(cpu.edx == vaddr_read(cpu.esp - 12, 4));
-	assert(cpu.ebx == vaddr_read(cpu.esp - 16, 4));
+	assert(cpu.edi == vaddr_read(cpu.esp     , 4));
+	assert(cpu.esi == vaddr_read(cpu.esp - 4 , 4));
+	assert(cpu.ebp == vaddr_read(cpu.esp - 8 , 4));
 	// ignore cpu.esp
-	assert(cpu.ebp == vaddr_read(cpu.esp - 24, 4));
-	assert(cpu.esi == vaddr_read(cpu.esp - 28, 4));
-	assert(cpu.edi == vaddr_read(cpu.esp - 32, 4));
+	assert(cpu.ebx == vaddr_read(cpu.esp - 12, 4));
+	assert(cpu.edx == vaddr_read(cpu.esp - 16, 4));
+	assert(cpu.ecx == vaddr_read(cpu.esp - 24, 4));
+	assert(cpu.eax == vaddr_read(cpu.esp - 28, 4));
 #endif
 }
 
